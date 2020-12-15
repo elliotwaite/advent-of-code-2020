a07b99cde602ff03e2cd7162f242ac997854bd28
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2020-12-15 10:16:17 -0800

    Add day15

---
+++ day15.nim
@@ -0,0 +1,31 @@
+include prelude
+import sugar
+
+
+proc solve(inputNums: seq[int], numTurns: int): int =
+  var mem: Table[int, int]
+  for turn, num in inputNums[0 .. ^2]:
+    mem[num] = turn
+
+  result = inputNums[^1]
+  for turn in inputNums.high ..< numTurns - 1:
+    let nextNum = if result in mem: turn - mem[result] else: 0
+    mem[result] = turn
+    result = nextNum
+
+
+proc main =
+  let inputNums = collect(newSeq):
+    for num in "inputs/day15.txt".readFile.split(','):
+      num.parseInt
+
+  let answer1 = solve(inputNums, numTurns = 2020)
+  echo answer1
+  doAssert answer1 == 1522
+
+  let answer2 = solve(inputNums, numTurns = 30_000_000)
+  echo answer2
+  doAssert answer2 == 18234
+
+
+main()
