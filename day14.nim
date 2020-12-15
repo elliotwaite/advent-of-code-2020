7a98e93903acf36144a89b8d09a76ce0e7456f3d
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2020-12-15 09:40:11 -0800

    Update day14

--- day14.nim
+++ day14.nim
@@ -2,17 +2,37 @@ include prelude
 import bitops, sequtils, strscans
 
 
+proc floatingOrMasks(mask: string): seq[int] =
+  var numXsRemaining = mask.count('X')
+  var shiftAmounts: seq[(int, int)]
+  for i, c in mask:
+    if c == 'X':
+      dec numXsRemaining
+      shiftAmounts.add (numXsRemaining, mask.high - i)
+
+  let baseOrMask = mask.replace('X', '0').parseBinInt
+  var orMask: int
+  for compactMask in 0 ..< 1 shl shiftAmounts.len:
+    orMask = baseOrMask
+    for (compactMaskShiftAmount, addressShiftAmount) in shiftAmounts:
+      orMask = orMask or (
+        (compactMask and (1 shl compactMaskShiftAmount)) shl
+        (addressShiftAmount - compactMaskShiftAmount)
+      )
+    result.add orMask
+
+
 proc part1(lines: seq[string]): int =
   var mask: string
-  var maskAnd, maskOr, address, val: int
+  var andMask, orMask, address, val: int
   var mem: Table[int, int]
   for line in lines:
     if line.scanf("mask = $+", mask):
-      maskAnd = mask.replace('X', '1').parseBinInt
-      maskOr = mask.replace('X', '0').parseBinInt
+      andMask = mask.replace('X', '1').parseBinInt
+      orMask = mask.replace('X', '0').parseBinInt
 
     elif line.scanf("mem[$i] = $i", address, val):
-      mem[address] = (val and maskAnd) or maskOr
+      mem[address] = val and andMask or orMask
 
   for v in mem.values:
     result += v
@@ -20,31 +40,18 @@ proc part1(lines: seq[string]): int =
 
 proc part2(lines: seq[string]): int =
   var mask: string
-  var maskAnd, maskOr, address, val, curAddress: int
-  var shiftAmounts: seq[(int, int)]
+  var andMask, address, val: int
+  var orMasks: seq[int]
   var mem: Table[int, int]
   for line in lines:
     if line.scanf("mask = $+", mask):
-      maskAnd = not mask.replace('X', '1').parseBinInt
-      maskOr = mask.replace('X', '0').parseBinInt
-      shiftAmounts = @[]
-      var numXsRemaining = mask.count('X')
-      for i, c in mask:
-        if c == 'X':
-          dec numXsRemaining
-          shiftAmounts.add (numXsRemaining, mask.high - i)
+      andMask = not mask.replace('X', '1').parseBinInt
+      orMasks = floatingOrMasks(mask)
 
     elif line.scanf("mem[$i] = $i", address, val):
-      address = (address and maskAnd) or maskOr
-      for compactMask in 0 ..< 1 shl shiftAmounts.len:
-        curAddress = address
-        for (compactMaskShiftAmount, addressShiftAmount) in shiftAmounts:
-          curAddress = curAddress or (
-            (compactMask and (1 shl compactMaskShiftAmount)) shl
-            (addressShiftAmount - compactMaskShiftAmount)
-          )
-
-        mem[curAddress] = val
+      address = address and andMask
+      for orMask in orMasks:
+        mem[address or orMask] = val
 
   for v in mem.values:
     result += v
